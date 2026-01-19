329989dedc62dfb59040db017f4899c2f4043c5d
John <github.unruly743@silomails.com>
2026-01-19 18:14:27 +0000

    feat: update ESLint configuration and dependencies to enhance code quality and support

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,6 @@
+import js from '@eslint/js';
+import globals from 'globals';
+
 export default [
   // Ignore patterns (replaces .eslintignore)
   {
@@ -10,81 +13,29 @@ export default [
       '**/*.min.js'
     ]
   },
+  {
+    ...js.configs.recommended,
+    files: ['**/*.js']
+  },
   // Project JS files
   {
     files: ['**/*.js'],
     languageOptions: {
       ecmaVersion: 'latest',
-      sourceType: 'module'
+      sourceType: 'module',
+      globals: {
+        ...globals.node,
+        ...globals.es2021
+      }
     },
     linterOptions: {
       reportUnusedDisableDirectives: true
     },
     rules: {
       'no-unused-vars': ['warn', { args: 'none', ignoreRestSiblings: true }],
-      'no-constant-condition': ['warn', { checkLoops: false }],
-      'eqeqeq': ['warn', 'always'],
-      'curly': ['warn', 'all'],
-      'no-alert': 'error',
-      'no-eval': 'error',
-      'no-implied-eval': 'error',
-      'no-new-func': 'error',
-      'no-new-wrappers': 'error',
-      'no-octal': 'error',
-      'no-proto': 'error',
-      'no-return-await': 'error',
-      'no-shadow-restricted-names': 'error',
-      'no-throw-literal': 'error',
-      'no-unmodified-loop-condition': 'error',
-      'no-useless-catch': 'error',
-      'no-var': 'error',
-      'prefer-const': ['warn', { destructuring: 'all' }],
-      'prefer-template': 'warn',
-      'object-shorthand': ['warn', 'always'],
-      'no-useless-escape': 'warn',
-      'no-restricted-imports': [
-        'error',
-        {
-          paths: [
-            {
-              name: 'openai',
-              message: 'Use createOpenAIClient from src/utils/clients.js so configuration is centralized.'
-            },
-            {
-              name: 'twilio',
-              message: 'Use createTwilioClient from src/utils/clients.js; direct SDK use should be limited to TwiML helpers.'
-            },
-            {
-              name: 'fastify',
-              message: 'Fastify should only be initialized in index.js.'
-            }
-          ]
-        }
-      ]
-    }
-  },
-  {
-    files: ['src/utils/clients.js'],
-    rules: {
-      'no-restricted-imports': 'off'
-    }
-  },
-  {
-    files: ['src/testing/voice-test-runner.js'],
-    rules: {
-      'no-restricted-imports': 'off'
-    }
-  },
-  {
-    files: ['src/routes/sms.js'],
-    rules: {
-      'no-restricted-imports': 'off'
-    }
-  },
-  {
-    files: ['index.js'],
-    rules: {
-      'no-restricted-imports': 'off'
+      'no-empty': 'off',
+      'no-useless-escape': 'off',
+      'no-case-declarations': 'off'
     }
   }
 ];
